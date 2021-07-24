8c549af967767206e80cebad4a379f692e2115c1
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2021-07-24 10:49:36 +0900

    Changed CSP-Plugin to be applied when not in development mode.

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,31 @@ const htmlPlugin = require('html-webpack-plugin');
 const cspPlugin = require('csp-html-webpack-plugin');
 const miniCssExtPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    target:  'electron-renderer',
-    entry:   './src/index.tsx',
-    cache:   true,
-    mode:    "development",
-    devtool: "source-map",
-    resolve: { extensions: [".ts", ".tsx", ".js"] },
-    output:  { path: path.join(__dirname, "dist"), filename: "bundle.js" },
-    plugins: [
-        new miniCssExtPlugin(),
-        new htmlPlugin({filename: "index.html", template: "./index.html"}),
-        // Todo: Allow the use of styled-components without using unsafe-inline in environments locked down by CSP.
-        //new cspPlugin({"base-uri": ["'self'"], "object-src": ["'none'"], "script-src": ["'self'"],
-        //    "style-src": ["'self'"], "frame-src": ["'none'"], "worker-src": ["'none'"]})
-    ],
-    module:  { 
-        rules: [ 
-            { test: /\.tsx?$/, use: "ts-loader",}, 
-            { test: /\.css$/,  use: [ miniCssExtPlugin.loader, { loader: "css-loader" } ], },
-        ] 
-    },
+module.exports = (env) => {
+    return {
+        target:  'electron-renderer',
+        entry:   './src/index.tsx',
+        cache:   true,
+        mode:    env.development ? "development" : "production",
+        devtool: "source-map",
+        resolve: { extensions: [".ts", ".tsx", ".js"] },
+        output:  { path: path.join(__dirname, "dist"), filename: "bundle.js" },
+        plugins: [
+            new miniCssExtPlugin(),
+            new htmlPlugin({filename: "index.html", template: "./index.html"}),
+            function () {
+                if (!env.development) {
+                    // Todo: Allow the use of styled-components without using unsafe-inline in environments locked down by CSP.
+                    return new cspPlugin({"base-uri": ["'self'"], "object-src": ["'none'"], "script-src": ["'self'"],
+                        "style-src": ["'self'"], "frame-src": ["'none'"], "worker-src": ["'none'"]})
+                }
+            }
+        ],
+        module:  {
+            rules: [
+                { test: /\.tsx?$/, use: "ts-loader",},
+                { test: /\.css$/,  use: [ miniCssExtPlugin.loader, { loader: "css-loader" } ], },
+            ]
+        },
+    }
 };
